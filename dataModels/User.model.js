85589d0a97df62a614ba6b929a4d310fc155dd5c
lomatul <mahzabin@iut-dhaka.edu>
2023-12-27 19:08:12 +0600

    datamodels

---
+++ dataModels/User.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  profile_image: {
+    type: String,
+    default:'',
+  }
+
+
+
+  
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
